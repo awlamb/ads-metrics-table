d20a17e33229ec15c755ddd77089afffdbb76e29
awlamb <awill310@gmail.com>
2017-06-01 15:57:53 -0700

    hide table

--- table.jsx
+++ table.jsx
@@ -27,33 +27,33 @@ export default class AdsTable extends React.Component {
 	render() {
 		var dataList = this.props.metrics;
 		var ads = this.props.ads;
-		if(this.props.loaded) {
-			return (
-				<Table
-					rowHeight={50}
-					headerHeight={50}
-					rowsCount={dataList.rows.length}
-					width={this.state.width}
-					minWidth={500}
-					height={500}>
+		var loaded = this.props.loaded;
+		if(loaded) {
+		return (
+			<Table
+				rowHeight={50}
+				headerHeight={50}
+				rowsCount={dataList.rows.length}
+				width={this.state.width}
+				minWidth={500}
+				height={500}>
+					<Column
+						header={<Cell>Ad Name</Cell>}
+						cell={<NameCell data={dataList} col="remote_id" ads={ads} />}
+						fixed={true}
+						width={100}
+					/>
+					{ dataList.column_names.map( (column) =>
 						<Column
-							header={<Cell>Ad Name</Cell>}
-							cell={<NameCell data={dataList} col="remote_id" ads={ads} />}
-							fixed={true}
-							width={100}
+							header={<Cell>{column}</Cell>}
+							cell={<MetricCell data={dataList} col={column} />}
+							width={200}
 						/>
-						{ dataList.column_names.map( (column) =>
-							<Column
-								header={<Cell>{column}</Cell>}
-								cell={<MetricCell data={dataList} col={column} />}
-								width={200}
-							/>
-						)}
-				</Table>
-			);
-		} else {
-			return (<span />
-			)
-		}
+					)}
+			</Table>
+		);
+	} else {
+		return (<div />)
+	}
 	}
 }
